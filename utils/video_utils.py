57244f8dcd6dd4fed27ed03eac69113ae8e170dd
ayoubarich-dev <ayoubarich36@gmail.com>
2025-12-06 23:59:12 +0100

    first commit

---
+++ utils/video_utils.py
@@ -0,0 +1,21 @@
+import cv2
+
+def read_video(video_path):
+    """Reads a video from the given path and returns the frames as a list."""
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    cap.release()
+    return frames
+
+
+def save_video(ouput_video_frames, output_video_path):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_video_path, fourcc,24, (ouput_video_frames[0].shape[1], ouput_video_frames[0].shape[0]))
+    for frame in ouput_video_frames:
+        out.write(frame)
+    out.release()
