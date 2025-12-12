b2cf68f6b0cb1afa0c381240ba63f9e500d0d7d7
ayoubarich-dev <ayoubarich36@gmail.com>
2025-12-12 21:11:36 +0100

    add Assign Ball Aquisition

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from utils import read_video, save_video
 from trackers import Tracker
-
+from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
+from tqdm import tqdm
+import numpy as np
 
 def main():
     #read video
@@ -16,6 +19,37 @@ def main():
     
     tracks = tracker.get_object_trackes(video_frames,read_from_stub=True,stub_path="stubs/track_stubs.pkl")
     
+    # Interpolate Ball Positions
+    print("🧮 Interpolating ball positions...")
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+    #Assign team colors
+    print("🎨 Assigning team colors...")
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0],
+                                    tracks["players"][0]
+                                    )
+    for frame_num, player_track in enumerate(tqdm(tracks["players"], desc="Assigning team colors to players")):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+            
+    # Assign Ball Aquisition
+    player_assigner =PlayerBallAssigner()
+    team_ball_control= []
+    for frame_num, player_track in enumerate(tqdm(tracks['players'], desc="Assigning ball to players")):
+        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
+            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+        else:
+            team_ball_control.append(team_ball_control[-1])
+    team_ball_control= np.array(team_ball_control)
+
     #Draw output 
     ##Draw object Tracks
     
--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,5 +1,5 @@
 import cv2
-
+from tqdm import tqdm
 def read_video(video_path):
     """Reads a video from the given path and returns the frames as a list."""
     cap = cv2.VideoCapture(video_path)
@@ -16,6 +16,6 @@ def read_video(video_path):
 def save_video(ouput_video_frames, output_video_path):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_video_path, fourcc,24, (ouput_video_frames[0].shape[1], ouput_video_frames[0].shape[0]))
-    for frame in ouput_video_frames:
+    for frame in tqdm (ouput_video_frames, desc="Saving video"):
         out.write(frame)
     out.release()
