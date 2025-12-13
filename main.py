e5b488911b162e6601c6b5d2112d47c0756235e9
ayoubarich-dev <ayoubarich36@gmail.com>
2025-12-13 22:01:30 +0100

    add speed and distance

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from utils import read_video, save_video
 from trackers import Tracker
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
+from camera_movement_estimator import CameraMovementEstimator
+from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistance_Estimator
 from tqdm import tqdm
 import numpy as np
 
@@ -19,9 +22,33 @@ def main():
     
     tracks = tracker.get_object_trackes(video_frames,read_from_stub=True,stub_path="stubs/track_stubs.pkl")
     
+    # Get object positions 
+    tracker.add_position_to_tracks(tracks)
+
+    # camera movement estimator
+    print("📹 Estimating camera movement and adjusting object positions...")
+    camera_movement_estimator = CameraMovementEstimator(video_frames[0])
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,
+                                                                                read_from_stub=True,
+                                                                                stub_path='stubs/camera_movement_stub.pkl')
+    print("🔄 Adjusting object positions for camera movement...")
+    camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
+
+    # View Trasnformer
+    print("🔄 Transforming player and ball positions to top-down view...")
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
+    
     # Interpolate Ball Positions
     print("🧮 Interpolating ball positions...")
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+    
+    # Speed and distance estimator
+    print("🚀 Estimating speed and distance covered by players...")
+    
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
+    
     #Assign team colors
     print("🎨 Assigning team colors...")
     team_assigner = TeamAssigner()
@@ -37,6 +64,7 @@ def main():
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
             
     # Assign Ball Aquisition
+    print("🏆 Assigning ball possession to players...")
     player_assigner =PlayerBallAssigner()
     team_ball_control= []
     for frame_num, player_track in enumerate(tqdm(tracks['players'], desc="Assigning ball to players")):
@@ -55,8 +83,15 @@ def main():
     
     print("🖌️ Drawing annotations on video frames...")
     
-    output_video_frames = tracker.draw_annotations(video_frames,tracks)
+    output_video_frames = tracker.draw_annotations(video_frames,tracks,team_ball_control)
     
+    ## Draw Camera movement
+    print("🎥 Drawing camera movement annotations on video frames...")
+    output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
+    
+    ## Draw Speed and Distance
+    print("🏃‍♂️ Drawing speed and distance annotations on video frames...")
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames,tracks)
     #save video
     print("💾 Saving output video...")
     
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -9,12 +9,23 @@ import numpy as np
 import pandas as pd
 
 sys.path.append('../')
-from utils import get_center_of_bbox, get_bbox_width
+from utils import get_center_of_bbox, get_bbox_width, get_foot_position
 
 class Tracker:
     def __init__(self,model_path):
         self.model = YOLO(model_path, verbose=False)
         self.tracker = sv.ByteTrack()
+        
+    def add_position_to_tracks(sekf,tracks):
+        for object, object_tracks in tracks.items():
+            for frame_num, track in enumerate(object_tracks):
+                for track_id, track_info in track.items():
+                    bbox = track_info['bbox']
+                    if object == 'ball':
+                        position= get_center_of_bbox(bbox)
+                    else:
+                        position = get_foot_position(bbox)
+                    tracks[object][frame_num][track_id]['position'] = position
         
     def interpolate_ball_positions(self,ball_positions):
         ball_positions = [x.get(1,{}).get('bbox',[]) for x in ball_positions]
@@ -149,7 +160,27 @@ class Tracker:
         cv2.drawContours(frame, [triangle_points],0,(0,0,0), 2)
 
         return frame
-    def draw_annotations(self,video_frames,tracks):
+    
+    def draw_team_ball_control(self,frame,frame_num,team_ball_control):
+        # Draw a semi-transparent rectaggle 
+        overlay = frame.copy()
+        cv2.rectangle(overlay, (1350, 850), (1900,970), (255,255,255), -1 )
+        alpha = 0.4
+        cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+
+        team_ball_control_till_frame = team_ball_control[:frame_num+1]
+        # Get the number of time each team had ball control
+        team_1_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==1].shape[0]
+        team_2_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==2].shape[0]
+        team_1 = team_1_num_frames/(team_1_num_frames+team_2_num_frames)
+        team_2 = team_2_num_frames/(team_1_num_frames+team_2_num_frames)
+
+        cv2.putText(frame, f"Team 1 Ball Control: {team_1*100:.2f}%",(1400,900), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+        cv2.putText(frame, f"Team 2 Ball Control: {team_2*100:.2f}%",(1400,950), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+
+        return frame
+    
+    def draw_annotations(self,video_frames,tracks,team_ball_control):
         output_frames_frames= []
         for frame_num, frame in enumerate(tqdm(video_frames, desc="Drawing annotations")):
             frame = frame.copy()
@@ -171,5 +202,6 @@ class Tracker:
             for track_id, ball in ball_dict.items():
                 frame = self.draw_traingle(frame, ball["bbox"],(0,255,0))
             
+            frame = self.draw_team_ball_control(frame, frame_num, team_ball_control)
             output_frames_frames.append(frame)
         return output_frames_frames
